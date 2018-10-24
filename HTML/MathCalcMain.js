ff30e647c36c52ef8914f35ac54973d13691aa1f
Chapman Nils, GHR-OSA-NEX-15 <Nils.Chapman@swisscom.com>
2018-10-24 14:33:05 +0200

    JS

---
+++ HTML/MathCalcMain.js
@@ -0,0 +1,51 @@
+
+//Seiten mit Sinus berechnen
+function Calcb(seitea,winkelb,winkela) {
+
+    document.write("Solution: ");
+
+    var solution = seitea*Math.sin(winkelb)/Math.sin(winkela);
+    document.write(solution.toFixed(2));
+}
+
+
+function Calca(seiteb,winkela,winkelb) {
+    document.write("Solution: ");
+
+    var solution = seiteb*Math.sin(winkela)/Math.sin(winkelb);
+    document.write(solution.toFixed(2));
+}
+
+
+function calcc(seiteb,winkelc,winkelb) {
+
+    document.write("Solution: ");
+
+    var solution = seiteb*Math.sin(winkelc)/Math.sin(winkelb);
+    document.write(solution.toFixed(2));
+}
+
+//Winkel mit sinus berechnen
+function ClacWinka(seitea,winkelb,winkela) {
+
+    document.write("Solution: ");
+
+    var solution = seitea*Math.sin(winkelb)/Math.sin(winkela);
+    document.write(solution.toFixed(2));
+}
+
+function ClacWinkb(seitea,winkelb,winkela) {
+
+    document.write("Solution: ");
+
+    var solution = seitea*Math.sin(winkelb)/Math.sin(winkela);
+    document.write(solution.toFixed(2));
+}
+
+function ClacWinkc(seitea,winkelb,winkela) {
+
+    document.write("Solution: ");
+
+    var solution = seitea*Math.sin(winkelb)/Math.sin(winkela);
+    document.write(solution.toFixed(2));
+}
